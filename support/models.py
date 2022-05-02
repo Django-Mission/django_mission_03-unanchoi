be242e468dfdea4fe2fbc6e83a03859498712f46
unanchoi <unusualc@likelion.org>
2022-05-02 21:22:56 +0900

    feat : Add 질문, 문의, 답변 model 생성

--- support/models.py
+++ support/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Faq(models.Model):
+
+    choice_options = [
+        ("Django" , "Django"),
+        ("Python" , "Python"),
+        ("User" , "User"),
+        ("Etc" , "Etc")
+    ]
+
+
+    title = models.CharField(verbose_name ="작성자", max_length=20)
+    category = models.CharField(verbose_name="종류", 
+    max_length=20, 
+    default = "Python",
+    choices=choice_options
+)
+    content = models.TextField(verbose_name="내용")
+    edited_at = models.DateTimeField(auto_add = True)
+    
+    answer = models.ForeignKey(to=Answer, on_delete=models.CASCADE, null=True, blank=True)
+
+
+
+class Answer(models.Model):
+    answer = models.TextField()
+class Inquiry(models.Model):
+    choice_options = [
+        ("Django" , "Django"),
+        ("Python" , "Python"),
+        ("User" , "User"),
+        ("Etc" , "Etc")
+    ]
+ 
+    title = models.CharField(verbose_name ="작성자", max_length=20)
+    writer = models.CharField(verbose_name ="작성자", max_length=20)
+    category = models.CharField(verbose_name="종류",
+    max_length=20,
+    choices=choice_options)
+    created_at = models.DateTimeField(created_at)
