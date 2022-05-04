a76fa57828d8bb26c813a227dae3359763997d09
unanchoi <unusualc@likelion.org>
2022-05-04 19:11:00 +0900

    feat : admin 등록

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,9 @@
+from tabnanny import verbose
 from django.contrib import admin
 
+from .models import Faq, Inquiry, Answer
 # Register your models here.
+
+admin.site.urls(Faq)
+admin.site.urls(Inquiry)
+admin.site.urls(Answer)
